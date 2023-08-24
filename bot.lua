7e7d0010c1f46201677b79e792e713da9e0a35be
Chris <xcanary8404@gmail.com>
2023-08-23 23:51:36 -0700

    Upload

---
+++ bot.lua
@@ -0,0 +1,20 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+
+local secretFile = io.open("secret")
+
+if secretFile == nil then
+    error("No secret file.")
+end
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+	if message.content == '!ping' then
+		message.channel:send('Pong!')
+	end
+end)
+
+client:run("Bot "..secretFile:read("*a"))
